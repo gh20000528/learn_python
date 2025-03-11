7e4cc4b0fc92a34ed6f1be4bed89799af2dd18db
han sheng <gh890528@gmail.com>
2025-03-11 14:53:43 +0800

    day 06

--- day_5.py
+++ day_5.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels = set("aeiouAEIOU")
-        stack = [char for char in s if char in vowels]
+        stack = [char for char in s if char in vowels] # 檢查 str 中有沒有母音部分有加入 stack
         result = []
 
         for char in s:
@@ -12,3 +12,8 @@ class Solution:
 
 
         return "".join(result) 
+
+
+
+# 利用 for 迴圈加上 stack 將含有 aeiouAEIOU 字母部分加入 stack 
+# 再用 for 迴圈將有字母的部分 pop 出來做替換
